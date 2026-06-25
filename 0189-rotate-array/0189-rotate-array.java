bfaac00d147a42cc3ec58423e9509a53b953ad4e
hs380102-blip <hs380102@gmail.com>
2026-06-25 12:13:57 +0530

    Time: 1 ms (40.94%), Space: 63.2 MB (16.37%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+
+        int n = nums.length;
+        k = k % n;
+
+        int[] temp = new int[k];
+
+        // Store last k elements
+        for (int i = 0; i < k; i++) {
+            temp[i] = nums[n - k + i];
+        }
+
+        // Shift remaining elements right
+        for (int i = n - k - 1; i >= 0; i--) {
+            nums[i + k] = nums[i];
+        }
+
+        // Put saved elements at front
+        for (int i = 0; i < k; i++) {
+            nums[i] = temp[i];
+        }
+    }
+}
