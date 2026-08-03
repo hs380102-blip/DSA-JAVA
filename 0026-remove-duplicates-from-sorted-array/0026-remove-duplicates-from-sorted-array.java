285d278d44b5054045c1d8c1e274d2b9ddd78f10
hs380102-blip <hs380102@gmail.com>
2026-08-03 11:02:47 +0530

    Time: 1 ms (77.18%), Space: 46.6 MB (82.63%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int removeDuplicates(int[] nums) {
+    public int  removeDuplicates(int[] nums) {
         int j=1;
         for(int k=0;k<nums.length;k++){
             if(nums[k]!=nums[j-1]){
