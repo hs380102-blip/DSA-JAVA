a1d2d653262bce6a4dc37beb789a2f5ba43de4f3
hs380102-blip <hs380102@gmail.com>
2026-09-24 15:21:02 +0530

    Time: 2 ms (91.65%), Space: 47.6 MB (87.56%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,21 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n=nums.length;
-        
         int j=0;
-        int temp[]=new int[n];
-        for(int i=0;i<n;i++){
-            if(nums[i]!=0){
-                temp[j]=nums[i];
-                j++;
-            }
-        }
-        for(int i=0;i<n;i++){
-            nums[i]=temp[i];
-        }
+        for(int i=0;i<nums.length;i++){
 
-            
-
+        
+        if(nums[i] !=0){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            j++;
+        }
     }
-    
+}
 }
